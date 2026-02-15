81661ca15a29240b62a0f15ceacbdea681981d2f
Mike <124715146+Nasokom@users.noreply.github.com>
2026-02-15 18:58:50 +0100

    serving phonebook front  prod build with express

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body',{
 }))
